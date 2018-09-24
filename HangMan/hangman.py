3ebe3d254209ad6e867340908566d525598002b7
bq666 <wojciech.gaudnik@gmail.com>
2018-09-24 12:42:44 +0200

    1

--- HangMan/hangman.py
+++ HangMan/hangman.py
@@ -1,22 +1,51 @@
-word = 'wielka dupa jaś sałata'
-b = 'z'
+import sys
+from _cffi_backend import string
+
+word = 'dupa jas salata'
 long = len(word)
 
 dashed_word = ''
-while len(dashed_word) < long
+for chr in word:
+	if chr != ' ':
+		dashed_word += '_'
+	else:
+		dashed_word += ' '
 
-
-dashed_word = str(['_' for a in word])
-
+print(word)
 print(dashed_word)
 
+word = list(word)
+dashed_word = list(dashed_word)
 
-while b != '*':
-	b = input("leter:")
-	while b in a:
-		a = a.replace(b, '*')
+b = '*'
+while b:
+	b = 'a'  # input("leter:")
+	while b in word:
+		num = word.index(b)
+		dashed_word[num] = b
+		word[num] = '*'
+	b = ''
 
+# print(type(word))
+word = string.join(word, '')
+print(word)
+print(dashed_word)
 
+# b = '*'
+# while b:
+# 	b = 'a' #input("leter:")
+# 	while b in word:
+# 		num = word.index(b)
+# 		dashed_word = list(dashed_word)
+# 		word = list(word)
+# 		word[num] = '*'
+# 		word = str(word)
+# 		dashed_word[num] = b
+# 		# word = word.replace(b, '*')
+# 		# dashed_word = dashed_word.
+# 	# b = ''
+# 	# sys.exit()
+# print(word)
+# print(dashed_word)
 
-print(a)
 
