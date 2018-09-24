77535c54680fbcbfda182a034fef938482182c10
bq666 <wojciech.gaudnik@gmail.com>
2018-09-24 12:47:11 +0200

    2

--- HangMan/hangman.py
+++ HangMan/hangman.py
@@ -14,38 +14,20 @@ for chr in word:
 print(word)
 print(dashed_word)
 
-word = list(word)
-dashed_word = list(dashed_word)
+
 
 b = '*'
 while b:
-	b = 'a'  # input("leter:")
+	b = input("leter:")
 	while b in word:
+		word = list(word)
+		dashed_word = list(dashed_word)
 		num = word.index(b)
 		dashed_word[num] = b
 		word[num] = '*'
-	b = ''
-
-# print(type(word))
-word = string.join(word, '')
-print(word)
-print(dashed_word)
-
-# b = '*'
-# while b:
-# 	b = 'a' #input("leter:")
-# 	while b in word:
-# 		num = word.index(b)
-# 		dashed_word = list(dashed_word)
-# 		word = list(word)
-# 		word[num] = '*'
-# 		word = str(word)
-# 		dashed_word[num] = b
-# 		# word = word.replace(b, '*')
-# 		# dashed_word = dashed_word.
-# 	# b = ''
-# 	# sys.exit()
-# print(word)
-# print(dashed_word)
+	word = ''.join(word)
+	dashed_word = ''.join(dashed_word)
+	print(word)
+	print(dashed_word)
 
 
